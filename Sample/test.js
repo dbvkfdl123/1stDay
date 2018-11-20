2bed3f5ce86aad2c15acb6c05e71c0f1f046374b
dbvkfdl123 <dbvkfdl123@gmail.com>
2018-11-20 14:10:45 +0900

    조건문

---
+++ Sample/test.js
@@ -0,0 +1,41 @@
+// var name = 'Yu';
+
+// if(name == 'yu'){
+//     console.log("My name is yu.");
+// }
+// else if (name == 'Yu'){
+//     console.log("My name is Yu.");
+// }
+
+// for(var i =0 ; i<5 ;i++)
+// {
+//     console.log("i = %d",i);
+// }
+// let time1 = 100
+// while (time1 > 100)
+// {
+//     console.logt("Result: %d", time1);
+//     time1--;
+// }
+
+// let time2 = 100;
+// do {
+//     console.log("Result: %d", time2);
+//     time2;
+// }
+// while(time2 > 100)
+
+//switch
+let action ="eat";
+switch(action)
+{
+    case 'draw':
+        console.log('draw');
+        break;
+    case 'eat':
+        console.log('eat');
+        break;
+    default:
+        console.log('nothing');
+        break;
+}
